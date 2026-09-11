ee311b2866c9b556f4ef19d43913f81929a19dd7
Ramesh Katakam <katakamramesh52@gmail.com>
2026-09-12 01:38:10 +0300

    tempature and system role

---
+++ week1/day2/sys_temp.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key missed")
+
+client=Groq(api_key=my_api_key)
+
+model="openai/gpt-oss-120b"
+role="user"
+prompt="i love you babes"
+
+message_system={
+    "role":"system",
+    "content": "You are my loving wife"
+}
+
+message={
+    "role" : role,
+    "content" : prompt
+}
+
+messages=[message_system,message]
+# Temperature by default is 0 meaning safe. range is [0,2]
+response=client.chat.completions.create(model=model, messages=messages)
+# print(response)
+
+print("--------------------")
+
+answer=response.choices[0].message.content
+print(answer)
+
+print("--------------------")
