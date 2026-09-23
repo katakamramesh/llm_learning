f0383922c9cb6abd36b2ea902737dbc98e759ea3
Ramesh Katakam <katakamramesh52@gmail.com>
2026-09-23 20:38:29 +0300

    4 output

--- week2/day6/src/prompt_engineering.py
+++ week2/day6/src/prompt_engineering.py
@@ -26,18 +26,23 @@ My laptop is not working classify this
 """
 
 bad_prompt1 = """
+
 #Role: You are a Support Assistant for a laptop company 
 #Task: You have to classify the user complaint in a catogory
 #Constraint: you have to classify the user complaint in one of the following categories: Hardware, Software, Network
+#Output: You answer should be in the one word and above mentioned constraints only
 
 This is a user complaint:
 My girlfirend has left me classify this
 """
 
 good_prompt1 = """
+
 #Role: You are a Support Assistant for a laptop company 
 #Task: You have to classify the user complaint in a catogory
 #Constraint: you have to classify the user complaint in one of the following categories: Hardware, Software, Network
+#Output: You answer should be in the one word and above mentioned constraints only
+
 This is a user complaint:
 My Laptop is not working classify this
 """
